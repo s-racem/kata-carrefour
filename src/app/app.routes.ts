288c35e56eed1d9c5a0213004ee7e2732b55cc26
sracem <s.racem@coconsult.fr>
2024-03-25 23:49:00 +0100

    User story 1: fetch and display the list of products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {ProductsComponent} from "./components/products/products.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products'
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    component: ProductsComponent,
+  }
+];
